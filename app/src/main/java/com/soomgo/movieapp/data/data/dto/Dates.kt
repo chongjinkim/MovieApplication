4b114113bd3a3edfae6e4931e4ce279d08300248
김종진 <martia0118@gmail.com>
2021-10-10 14:56:11 +0900

    DataResponse 선언 완료

---
+++ app/src/main/java/com/soomgo/movieapp/data/data/dto/Dates.kt
@@ -0,0 +1,6 @@
+package com.soomgo.movieapp.data.data.dto
+
+data class Dates(
+    val maximum: String,
+    val minimum: String
+)
--- app/src/main/java/com/soomgo/movieapp/data/data/dto/Result.kt
+++ app/src/main/java/com/soomgo/movieapp/data/data/dto/Result.kt
@@ -1,28 +1,18 @@
 package com.soomgo.movieapp.data.data.dto
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class MovieDTO(
+data class Result(
     val adult: Boolean,
     val backdrop_path: String,
-    val budget: Int,
-    val homepage: String,
+    val genre_ids: List<Int>,
     val id: Int,
-    val imdb_id: String,
     val original_language: String,
     val original_title: String,
     val overview: String,
     val popularity: Double,
     val poster_path: String,
     val release_date: String,
-    val revenue: Int,
-    val runtime: Int,
-    val status: String,
-    val tagline: String,
     val title: String,
     val video: Boolean,
     val vote_average: Double,
     val vote_count: Int
-) : Parcelable
+)
