626764db53d4f1d37630f65ef9d23937eccb7d9d
김종진 <martia0118@gmail.com>
2021-10-23 17:33:24 +0900

    API DATABASE주소 접근 완료

--- app/src/main/java/com/soomgo/movieapp/data/local/MovieDAO.kt
+++ app/src/main/java/com/soomgo/movieapp/data/local/MovieDAO.kt
@@ -2,6 +2,7 @@ package com.soomgo.movieapp.data.local
 
 import androidx.room.*
 import com.soomgo.movieapp.domain.model.Movie
+import com.soomgo.movieapp.domain.model.TV
 import kotlinx.coroutines.flow.Flow
 
 @Dao
--- app/src/main/java/com/soomgo/movieapp/data/network/tmdbAPI.kt
+++ app/src/main/java/com/soomgo/movieapp/data/network/tmdbAPI.kt
@@ -1,14 +1,12 @@
 package com.soomgo.movieapp.data.network
 
-import com.soomgo.movieapp.data.dto.DetailResponse
-import com.soomgo.movieapp.data.dto.MovieResponse
-import com.soomgo.movieapp.data.dto.UpComingResponse
+import com.soomgo.movieapp.data.dto.*
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface tmdbAPI {
 
-//api접속할 주소 가져옴,
+    //api접속할 주소 가져옴,
 //api 정보 받아 올 떄 키와 토큰값이 반드시 있어야 한다.
 //api정모 가져올때 키, 토큰 둘다 필요 !
     @GET("/3/movie/popular")
@@ -25,6 +23,15 @@ interface tmdbAPI {
         @Path("id") id : String
     ) : DetailResponse
 
+    @GET("/3/tv/popular")
+    suspend fun getPopularTv() : TVResponse
 
+    @GET("/3/tv/top_rated")
+    suspend fun getTopRatedTv() : TVResponse
 
+    @GET("/3/tv/on_the_air")
+    suspend fun getOnTheAirTv() : TVOnTheAirResponse
+
+    @GET("/3/tv/{id}")
+    suspend fun getDetailTv(@Path("id") id : String) : TVDetailResponse
 }
